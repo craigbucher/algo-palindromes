c7854c1de65c099981c2053a83dde0552f72c764
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-26 16:42:59 -0500

    working js

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,22 @@
 exports.palindrome = function(word) {
+    // ensure word is a string
+    word = word.toString();
+    // globally (/g) replace/remove chars not a-z, A-Z or 0-9	
+    let tmpWord = word.replace(/[^a-zA-Z0-9]/g, '');
+    // convert to lower case
+    string = tmpWord.toLowerCase();
+    // a single letter is technically a palindrome
+    if (string.length === 1){	
+      return true;	
+    }
+    // call the helper function to check if string is equal to its reverse	
+    return (string === revString(string));	
+};    
+// helper function     
+function revString(str){	
+    strArr = str.split('');	
+    arrRev = strArr.reverse();	
+    return arrRev.join('');	
+}
 
-};
+// console.log(exports.palindrome('nice'))
